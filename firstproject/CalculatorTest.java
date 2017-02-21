80afa55ac31160360fb2afa977a674661a1f0155
jackesafonso <afonsogoncalves@msn.com>
2017-02-21 17:36:06 +0000

    G-unit test

--- firstproject/CalculatorTest.java
+++ firstproject/CalculatorTest.java
@@ -2,14 +2,24 @@ package com.fdmgroup.firstproject;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculatorTest {
+	
+	private static Calculator calc = new Calculator(); // was declared here because it was a local variable on 
+	                                                  //setup and could not be accessed by entire class
+	
+	@Before
+	public void setup(){
+		calc = new Calculator();
+	}
 
 	@Test
 	public void testThatWhenIAddOnePlusOneItReturnsTwo() {
 
-		Calculator calc = new Calculator();
+		
 		double result = calc.add(1, 1);
 		assertEquals(2, result, 0.01);
 
@@ -18,7 +28,7 @@ public class CalculatorTest {
 	@Test
 	public void testThatWhenISubstractTwoFromFiveItReturnsThree(){
 		
-		Calculator calc = new Calculator();
+		
 		double result = calc.subtract(5,2);
 		assertEquals(3,result,0.01);
 		
