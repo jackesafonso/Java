5c9fa23ae66d64485bb8ceadf126b99551d0aeae
jackesafonso <afonsogoncalves@msn.com>
2017-02-27 09:14:11 +0000

    Javaweek1assessment

--- javaWeek1Assessment/GradeCalculator.java
+++ javaWeek1Assessment/GradeCalculator.java
@@ -2,16 +2,30 @@ package com.fdmgroup.javaWeek1Assessment;
 
 public class GradeCalculator {
 	
-	public String pass;
-	
+    private String result;
 
-	private String fail;
+    public String getClassification(double mark) {
+    
+           
+           
+           if (mark>90){
+                  result = "distinction";
+                  
+           }else if (mark>80){
+                  result =  "merit";
+                  
+           }else if (mark>=75){
+                  result = "pass";
+           
+           }else if(mark<75){
+                  result = "fail";
+                  
+           }return result;
+    
+    }
 
-	public String getClassification(double mark) {
-          if(mark < 75);
-		return fail;
-	}
 
 	
 }
 
+      
